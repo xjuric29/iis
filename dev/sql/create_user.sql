3f45fdc48d93ca6de6f72b5df37dac3782fc4865
Jiří Juřica <xjuric29@vutbr.cz>
2019-10-12 20:51:25 +0200

    Adding database init sql.

---
+++ dev/sql/create_user.sql
@@ -0,0 +1,10 @@
+# ISS project
+# Description: Create project db user.
+# Author: Jiri Jurica (xjuric29)
+
+CREATE USER 'iis'@'localhost' IDENTIFIED BY 'the_best_project_ever';
+GRANT ALL PRIVILEGES ON `iis`.* TO 'iis'@localhost IDENTIFIED BY 'the_best_project_ever';
+FLUSH PRIVILEGES;
+
+# If you need to remove this user.
+# DELETE FROM `mysql`.`user` WHERE `user` = 'iis' and `host` = 'localhost';
