274f118e471074af10f50e6dfcba52c8c05d3e22
Michal Pospíšil <xpospi95@stud.fit.vutbr.cz>
2019-11-27 03:34:02 +0100

    Make ordering persistent

--- web-project/app/Presenters/TicketsPresenter.php
+++ web-project/app/Presenters/TicketsPresenter.php
@@ -10,6 +10,12 @@ class TicketsPresenter extends Nette\Application\UI\Presenter
     /** @var \App\Model\Tickets @inject */
     public $tickets;
 
+    /** @persistent */
+    public $orderBy;
+
+    /** @persistent */
+    public $orderDir;
+
     public function renderDefault($orderBy, $orderDir, $page = 1): void
     {
         $this->template->ticketList = $this->tickets->getTicketTable($orderBy, $orderDir, $page);
