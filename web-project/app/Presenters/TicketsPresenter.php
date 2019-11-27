e6f64330ba3fae8c331ae246905dab33ae9f9689
Michal Pospíšil <xpospi95@stud.fit.vutbr.cz>
2019-11-27 22:25:24 +0100

    Add documentation

--- web-project/app/Presenters/TicketsPresenter.php
+++ web-project/app/Presenters/TicketsPresenter.php
@@ -1,5 +1,8 @@
 <?php
-
+/** Presenter for listing tickets
+ * @author Michal Pospíšil
+ * @email xpospi95@stud.fit.vutbr.cz
+ */
 namespace App\Presenters;
 
 use Nette;
@@ -21,6 +24,8 @@ class TicketsPresenter extends Nette\Application\UI\Presenter
      * @param $orderBy: ordering parameter, name and date for now, values defined in this function
      * @param $orderDir: ordering direction, desc(ending) or asc(ending)
      * @param $page: number of page to render, first is default
+     * @param $search: key that is searched in name and description of ticket table
+     * @param $userid: filters ticket from this user
      */
     public function renderDefault($orderBy, $orderDir, $page = 1, $search = null, $userid = null): void
     {
@@ -30,7 +35,6 @@ class TicketsPresenter extends Nette\Application\UI\Presenter
 
     /** Creates a search bar
      * @author xpospi95
-     * @param $orderBy:
      */
     public function createComponentSearch() : Nette\Application\UI\Form {
         $form = new Nette\Application\UI\Form;
@@ -41,6 +45,12 @@ class TicketsPresenter extends Nette\Application\UI\Presenter
         return $form;
     }
 
+    /** Handles search request
+     * @author xpospi95
+     * @param $form: Nette Form object
+     * @param $values: values entered into form
+     * @throws ...
+     */
     public function performSearch(Nette\Application\UI\Form $form, \stdClass $values): void
     {
         $this->redirect('Tickets:', ['search' => $values->searchBox, 'page' => '1']);
