3cdf341dfdf2b25481f508a2dbf9643a39a80e21
Michal Pospíšil <xpospi95@stud.fit.vutbr.cz>
2019-11-29 20:22:29 +0100

    Implemented Jiri's changes pt.2

---
+++ web-project/app/model/ViewTasks.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Model;
+
+use Nette;
+
+class ViewTasks extends ListModel {
+    protected function selectSearched($search, Nette\Database\Table\Selection $table) {
+        if ($search) {
+            $search = '%' . $search . '%';
+            return $table->where("name LIKE ? OR description LIKE ?", $search, $search);
+        }
+        return $table;
+    }
+
+    protected function openTable($orderStr) {
+        return $this->database->table('task')
+            ->order($orderStr)
+            ->limit($this->paginator->getLength(), $this->paginator->getOffset());
+    }
+
+    protected function createOrderStr($orderBy)
+    {
+        switch($orderBy) {
+            case "name":
+            default:
+                return "name";
+        }
+    }
+
+    protected function filterByUser($userid, Nette\Database\Table\Selection $table) {
+        return $table->where("author LIKE ?", $userid);
+    }
+}
