b7aeb7243212d4bb3871e5c3e4bb7abe2ff3f949
Michal Pospíšil <xpospi95@stud.fit.vutbr.cz>
2019-11-29 20:22:29 +0100

    Implemented Jiri's changes pt.2

---
+++ web-project/app/model/ViewTickets.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Model;
+
+use Nette;
+
+class ViewTickets extends ListModel {
+    protected function selectSearched($search, Nette\Database\Table\Selection $table) {
+        if ($search) {
+            $search = '%' . $search . '%';
+            return $table->where("name LIKE ? OR description LIKE ?", $search, $search);
+        }
+        return $table;
+    }
+
+    protected function openTable($orderStr) {
+        return $this->database->table('ticket')
+            ->order($orderStr)
+            ->limit($this->paginator->getLength(), $this->paginator->getOffset());
+    }
+
+    protected function createOrderStr($orderBy)
+    {
+        switch($orderBy) {
+            case "name":
+                return "name";
+            case "date":
+            default:
+                return "creation_date";
+        }
+    }
+
+    protected function filterByUser($userid, Nette\Database\Table\Selection $table) {
+        return $table->where("author LIKE ?", $userid);
+    }
+
+    # xjuric29 methods
+    public function getTicket($id) {
+        /**Return data for specific ticket.
+           @param $id: Specific ticket id. */
+        $query = $this->database->table('ticket')->where('id = ?', $id);
+
+        return $query->fetch();
+    }
+}
